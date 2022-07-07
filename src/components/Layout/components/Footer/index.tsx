f27c8f080ebfdab4c8b4f660054a7ee0acbf98aa
TranTienQuy <quytran2296@gmail.com>
2022-07-07 16:26:53 +0700

    add content footer

--- src/components/Layout/components/Footer/index.tsx
+++ src/components/Layout/components/Footer/index.tsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import * as S from '@components/Layout/components/Footer/styles'
 
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = () => {
+  return <S.Wrapper></S.Wrapper>
+}
+
+export default Footer
